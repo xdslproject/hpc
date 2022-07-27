25e28030abd8ba18ebff75438c17f0d6e1be70d7
Nick Brown <nick@mesham.com>
2022-07-27 15:08:57 +0100

    PGAS dependencies lifted to loop level

--- psy/determine_pgas_dependencies.py
+++ psy/determine_pgas_dependencies.py
@@ -63,15 +63,15 @@ class ApplyPGASRewriter(RewritePattern):
 
         visitor = CollectAccessedVariables()
         for op in assignment.lhs.blocks[0].ops:
-          visitor.traverse(op)                
+          visitor.traverse(op)
         for key, value in visitor.accessed_variables.items():          
-          write_vars.append(psy_pgas.DataItem.get([value[0]], value[1]))
+          write_vars.append(psy_pgas.SingleDataItem.get([value[0]], value[1]))
 
         visitor.clear()
         for op in assignment.rhs.blocks[0].ops:
           visitor.traverse(op)  
         for key, value in visitor.accessed_variables.items():          
-          read_vars.append(psy_pgas.DataItem.get([value[0]], value[1]))
+          read_vars.append(psy_pgas.SingleDataItem.get([value[0]], value[1]))
 
         if len(read_vars) > 0 or len(write_vars) > 0:
           assignment.detach() 
@@ -89,21 +89,30 @@ class CollectIndexVariables(Visitor):
       self.traverse(op)
       
   def traverse_expr_name(self, id_expr: ftn_dag.ExprName): 
-    self.index_vars.append(id_expr.var)    
+    self.index_vars.append(id_expr.var)  
           
 class CollectDataRegions(Visitor):
   def __init__(self, loop_iterator_var):
     self.applicable_input_items=[]
     self.applicable_output_items=[]
     
-    self.loop_iterator_var=[loop_iterator_var]
+    self.loop_iterator_var=loop_iterator_var    
     self.loop_iterator_var_names=[]
     for v in self.loop_iterator_var:
-      loop_iterator_var_names.append(v.var_name.data)
+      self.loop_iterator_var_names.append(v.var_name.data)    
+      
+  def has_some_data_items(self):
+    return len(self.applicable_input_items) > 0 or len(self.applicable_output_items) > 0
+    
+  def traverse_call_expr(self, call_expr: ftn_dag.CallExpr):
+    print(call_expr.regions[0].blocks[0].ops)
+    for op in call_expr.bound_function_instance.blocks[0].ops:
+      self.traverse(op)
       
   def check_is_var_indexes_iterated_here(self, var):
     index_collector=CollectIndexVariables()
-    index_collector.traverse(var.indexes)      
+    for op in var.indexes.blocks[0].ops:
+      index_collector.traverse(op)      
     # For now just key on the name, might need to make more complex in future
     for index in index_collector.index_vars:
       if not index.var_name.data in self.loop_iterator_var_names: return False
@@ -112,16 +121,14 @@ class CollectDataRegions(Visitor):
   def traverse_data_region(self, data_region: psy_pgas.DataRegion):
     for var in data_region.inputs.blocks[0].ops:
       if self.check_is_var_indexes_iterated_here(var):
-        self.applicable_input_items.append(var)
+        self.applicable_input_items.append((data_region, var))
     for var in data_region.outputs.blocks[0].ops:
       if self.check_is_var_indexes_iterated_here(var):
-        self.applicable_output_items.append(var)       
-        
-        # store pair here, the data region and the data item - as will need to remove from data region and then check if empty or not (to remove entirely)
+        self.applicable_output_items.append((data_region, var))                       
           
 class CollectPGASDataRegions(RewritePattern):
   def __init__(self):
-    pass
+    self.processed_loop_vars=[]
     
   @op_type_rewrite_pattern
   def match_and_rewrite(  # type: ignore reportIncompatibleMethodOverride
@@ -129,38 +136,51 @@ class CollectPGASDataRegions(RewritePattern):
               
     block = do.parent
     idx = block.ops.index(do)
-    
-    print(do.iterator.blocks[0].ops[0].var.var_name.data)
-    visitor = CollectDataRegions(do.iterator.blocks[0].ops[0].var)
-    for op in do.body.blocks[0].ops:
+        
+    self.processed_loop_vars.append(do.iterator.blocks[0].ops[0].var)
+    visitor = CollectDataRegions(self.processed_loop_vars)
+    for op in do.body.blocks[0].ops:      
       visitor.traverse(op)
       
     read_vars=[]
     write_vars=[]
-    if len(visitor.data_regions) > 0:
-      for data_region in visitor.data_regions:
-        for op in data_region.inputs.blocks[0].ops:
-          read_vars.append(op)
-          op.detach()
-        for op in data_region.outputs.blocks[0].ops:
-          write_vars.append(op)
-          op.detach()
-        parent=data_region.parent
-        parent_idx=parent.ops.index(data_region)        
-        data_region.detach()
-        a=data_region.contents.blocks[0].ops[0]
-        data_region.contents.blocks[0].ops[0].detach()
-        rewriter.insert_op_at_pos(a, parent, parent_idx)
+    data_regions_being_worked_with=set()
+    if visitor.has_some_data_items():      
+      for entry in visitor.applicable_input_items:
+        read_vars.append(entry[1])        
+        entry[1].detach()
+        data_regions_being_worked_with.add(entry[0])
+      for entry in visitor.applicable_output_items:
+        write_vars.append(entry[1])        
+        entry[1].detach()
+        data_regions_being_worked_with.add(entry[0])
         
-      do.detach()      
-      combined_data_region = psy_pgas.DataRegion.get([do], read_vars, write_vars) 
-      rewriter.insert_op_at_pos(combined_data_region, block, idx)
+      for dr in list(data_regions_being_worked_with):
+        if len(dr.outputs.blocks[0].ops)==0 and len(dr.inputs.blocks[0].ops)==0:
+          # The Data region is no longer needed as all data items will be promoted to this level, therefore remove          
+          parent=dr.parent
+          parent_idx=parent.ops.index(dr)        
+          dr.detach()
+          a=dr.contents.blocks[0].ops[0]
+          dr.contents.blocks[0].ops[0].detach()
+          rewriter.insert_op_at_pos(a, parent, parent_idx)
+              
+      do_loop_blocks=[]      
+      for block in do.body.blocks:        
+        do.body.detach_block(block)        
+        do_loop_blocks.append(block)
+      combined_data_region = psy_pgas.DataRegion.get(do_loop_blocks, read_vars, write_vars)
+      block=Block()
+      block.add_op(combined_data_region)
+      do.body.add_block(block)      
 
 def apply_pgas_analysis(ctx: ftn_dag.MLContext, module: ModuleOp) -> ModuleOp:
     applyPGASRewriter=ApplyPGASRewriter()
     collector=CollectPGASDataRegions()
-    walker = PatternRewriteWalker(GreedyRewritePatternApplier([applyPGASRewriter, collector]), apply_recursively=False, walk_regions_first=True)
-    walker.rewrite_module(module)          
+    walker = PatternRewriteWalker(GreedyRewritePatternApplier([applyPGASRewriter]), apply_recursively=False, walk_regions_first=False)
+    walker.rewrite_module(module)
+    walker = PatternRewriteWalker(GreedyRewritePatternApplier([collector]), apply_recursively=False, walk_regions_first=False)
+    walker.rewrite_module(module)
     
-    print("a"+3)
+    #print("a"+3)
     return module
